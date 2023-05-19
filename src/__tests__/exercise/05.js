756beb58cc5b2cfc62b370489d70f15904589b13
Duy Tran <duy@seesaw.me>
2023-05-19 15:32:01 -0700

    Exercise 5 - Extra credit 4

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
+import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
 import {handlers} from '../../test/server-handlers'
@@ -19,6 +20,7 @@ const buildLoginForm = build({
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
@@ -56,5 +58,31 @@ test(`omitting the password displays an error`, async () => {
 
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
+  )
+})
+
+test(`does something when the server returns a 500 error`, async () => {
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(
+          ctx.status(500),
+          ctx.json({message: 'internal server error'}),
+        )
+      },
+    ),
+  )
+
+  render(<Login />)
+  const {username, password} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+  await waitForElementToBeRemoved(screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"internal server error"`,
   )
 })
