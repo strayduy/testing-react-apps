54949aa0ffda0b67fc687fec267b6d860658215d
Duy Tran <duy@seesaw.me>
2023-08-18 15:45:02 -0700

    Exercise 8 - Extra credit 3

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,62 +1,49 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function setup(props) {
-  let result = {};
+test('exposes the count and increment/decrement functions', () => {
+  const { result } = renderHook(() => useCounter());
 
-  function TestComponent() {
-    Object.assign(result, useCounter(props))
-    return null;
-  }
-
-  render(<TestComponent />);
-  return result;
-}
-
-test('exposes the count and increment/decrement functions', async () => {
-  const result = setup();
-
-  expect(result.count).toBe(0);
-  await act(result.increment);
-  expect(result.count).toBe(1);
-  await act(result.increment);
-  expect(result.count).toBe(2);
-  await act(result.decrement);
-  expect(result.count).toBe(1);
-  await act(result.decrement);
-  expect(result.count).toBe(0);
+  expect(result.current.count).toBe(0);
+  act(result.current.increment);
+  expect(result.current.count).toBe(1);
+  act(result.current.increment);
+  expect(result.current.count).toBe(2);
+  act(result.current.decrement);
+  expect(result.current.count).toBe(1);
+  act(result.current.decrement);
+  expect(result.current.count).toBe(0);
 })
 
-test('allows customization of the initial count', async () => {
-  const result = setup({ initialCount: 25 });
+test('allows customization of the initial count', () => {
+  const { result } = renderHook(() => useCounter({ initialCount: 25 }));
 
-  expect(result.count).toBe(25);
-  await act(result.increment);
-  expect(result.count).toBe(26);
-  await act(result.increment);
-  expect(result.count).toBe(27);
-  await act(result.decrement);
-  expect(result.count).toBe(26);
-  await act(result.decrement);
-  expect(result.count).toBe(25);
+  expect(result.current.count).toBe(25);
+  act(result.current.increment);
+  expect(result.current.count).toBe(26);
+  act(result.current.increment);
+  expect(result.current.count).toBe(27);
+  act(result.current.decrement);
+  expect(result.current.count).toBe(26);
+  act(result.current.decrement);
+  expect(result.current.count).toBe(25);
 })
 
-test('allows customization of the step', async () => {
-  const result = setup({ step: 3 });
+test('allows customization of the step', () => {
+  const { result } = renderHook(() => useCounter({ step: 3 }));
 
-  expect(result.count).toBe(0);
-  await act(result.increment);
-  expect(result.count).toBe(3);
-  await act(result.increment);
-  expect(result.count).toBe(6);
-  await act(result.decrement);
-  expect(result.count).toBe(3);
-  await act(result.decrement);
-  expect(result.count).toBe(0);
+  expect(result.current.count).toBe(0);
+  act(result.current.increment);
+  expect(result.current.count).toBe(3);
+  act(result.current.increment);
+  expect(result.current.count).toBe(6);
+  act(result.current.decrement);
+  expect(result.current.count).toBe(3);
+  act(result.current.decrement);
+  expect(result.current.count).toBe(0);
 })
 
 /* eslint no-unused-vars:0 */
